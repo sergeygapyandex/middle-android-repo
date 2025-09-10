d7c2e3eacf3fb5eac517c0084eda82bee70090a1
sergeygapyandex <sergey.gap.true@yandex.ru>
2025-09-11 02:42:41 +0300

    finish compose part

--- app/src/main/java/com/example/androidpracticumcustomview/ui/theme/MainScreen.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/ui/theme/MainScreen.kt
@@ -1,18 +1,19 @@
 package com.example.androidpracticumcustomview.ui.theme
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-
-/*
-Задание:
-Реализуйте необходимые компоненты.
-*/
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun MainScreen(closeActivity: () -> Unit) {
@@ -27,12 +28,26 @@ fun MainScreen(closeActivity: () -> Unit) {
 
             CustomContainerCompose(
                 firstChild = {
-                    // TODO
-                    // ...
+                    Text(
+                        text = "TopTV",
+                        modifier = Modifier
+                            .background(Color.Blue)
+                            .padding(16.dp),
+                        color = Color.White,
+                        fontSize = 22.sp,
+                        fontWeight = FontWeight.Bold
+                    )
                 },
                 secondChild = {
-                    // TODO
-                    // ...
+                    Text(
+                        text = "BottomTV",
+                        modifier = Modifier
+                            .background(Color.Red)
+                            .padding(16.dp),
+                        color = Color.White,
+                        fontSize = 22.sp,
+                        fontWeight = FontWeight.Bold
+                    )
                 }
             )
         }
